76ecc36d99d67bcbe230c32827ecfc8e754acd63
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2022-02-08 19:04:57 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/* */}
+      {/*</header>*/}
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+                <div className="col-1">Teste</div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-2">
+                    <Button variant='contained'>Teste</Button>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
